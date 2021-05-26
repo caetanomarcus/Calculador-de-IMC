8f52ee7fd7ae19ffc70d2543b4e7a507134c81ca
caetanomarcus <caetanomarcusfgmail.com>
2021-05-26 20:49:58 -0300

    código mais limpo, funções mais abstratas, comentários com o jsdocs

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+// VARIÁVEIS GLOBAIS
+
 let btn = document.getElementById('btn');
 let text = document.querySelector('p');
-let pesoInput = document.getElementById('peso');
-let alturaInput = document.getElementById('altura');
-let nomeInput = document.getElementById('nome');
-let idadeInput = document.getElementById('idade');
+
+/**
+ * Cálcula o imc
+ * 
+ * @param {Number} x Peso
+ * @param {Number} y Altura
+ * @returns {Number} imc = índice de massa corporal
+ */
 
 
-
-function IMC(peso, altura) {
-    peso = pesoInput.value;
-    altura = alturaInput.value;
-    imc = peso / (altura * altura);
-    return imc;
+function IMC(x, y) {
+  
+  return imc = x / y**2;
+   
 };
 
-function classification(imc) {
-    if (imc < 18.5) {
+/**
+ * Define a classificação do imc
+ * 
+ * @param {Number} z  imc
+ * @returns {String} a classificação do imc
+ */
+
+function classification(z) {
+    if (z < 18.5) {
         return "Magreza";
-    } else if (imc >= 18.5 && imc <= 24.9){
+    } else if (z >= 18.5 && z <= 24.9){
         return "Normal";
-    } else if (imc >= 25.0 && imc <= 29.9) {
+    } else if (z >= 25.0 && z <= 29.9) {
         return "Sobrepeso";
-    } else if (imc >= 30.0 && imc <= 39.9){
+    } else if (z >= 30.0 && z <= 39.9){
         return "Obesidade";
     } else {
         return "Obesidade Grave";
@@ -30,7 +41,15 @@ function classification(imc) {
 
 
 btn.addEventListener('click', function(event){
+    
     event.preventDefault();
+    
+    let person ={}
+    person.peso = +document.getElementById('peso').value;
+    person.altura = +document.getElementById('altura').value;
+    person.nome = document.getElementById('nome').value;
+    person.idade = +document.getElementById('idade').value;
+
     Table = document.querySelector('tbody');
     TR = document.createElement('tr');
     Table.appendChild(TR);
@@ -42,13 +61,10 @@ btn.addEventListener('click', function(event){
     for (var i = 0; i <= 3; i++){
         TR.appendChild(TDs[i]);
     };
-    TDs[0].innerHTML = nomeInput.value;
-    TDs[1].innerHTML = idadeInput.value;
-    IMC(peso, altura);
-    TDs[2].innerHTML = imc.toFixed(2).toString().replace(".",",");
+    TDs[0].innerHTML = person.nome;
+    TDs[1].innerHTML = person.idade;
+    IMC(person.peso, person.altura);
+    TDs[2].innerHTML = imc.toFixed(2).replace(".",",");
     TDs[3].innerHTML = classification(imc);
-
-   
-
 
 })
